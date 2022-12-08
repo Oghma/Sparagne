96140ad96d1ff06b9f6e390236f5b5ab0e359814
Matteo Lisotto <matteo.lisotto@gmail.com>
2022-12-08 23:12:40 +0100

    [entry] Add `id` field in `Entry`

--- src/engine/entry.rs
+++ src/engine/entry.rs
@@ -1,8 +1,11 @@
 //! The module contains the `Entry` type representing an entry in cash flows and wallets.
 //!
 //! Both expenses and income are represented by `Entry` type.
+use uuid::Uuid;
+
 #[derive(Debug)]
 pub struct Entry {
+    pub id: Uuid,
     pub amount: f64,
     pub category: String,
     pub note: String,
@@ -12,6 +15,7 @@ pub struct Entry {
 impl Entry {
     pub fn new(amount: f64, category: String, note: String) -> Self {
         Self {
+            id: Uuid::new_v4(),
             amount,
             category,
             note,
