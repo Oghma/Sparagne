7f487dc580885870c4b91436c8b5c347de75928f
Matteo Lisotto <matteo.lisotto@gmail.com>
2022-12-09 19:23:28 +0100

    [cash_flows] Move `Bounded` type in the new submodule

--- src/engine/cash_flows/bounded.rs
+++ src/engine/cash_flows/bounded.rs
@@ -1,68 +1,33 @@
-//! A collection of types that implement `CashFlow` trait.
-//!
-//! The available cash flows are:
-//! - [`UnBounded`]
-//! - [`Bounded`]
-//! - [`HardBounded`]
-use super::entry::Entry;
-use super::errors::EngineError;
-pub use bounded::Bounded;
-pub use unbounded::UnBounded;
+//! `Bounded` Cashflow type. The ideal type for cash flow that require an upper
+//! bound.
 
-mod bounded;
-mod unbounded;
+use super::{CashFlow, EngineError, Entry};
 
-/// `CashFlow` trait. Base requirements for a `CashFlow`.
-pub trait CashFlow {
-    fn add_entry(
-        &mut self,
-        balance: f64,
-        category: String,
-        note: String,
-    ) -> Result<(), EngineError> {
-        let entry = Entry::new(balance, category, note);
-        self.insert(entry)
-    }
-
-    fn archive(&mut self);
-    fn delete_entry(&mut self, id: &uuid::Uuid) -> Result<(), EngineError>;
-    fn update_entry(
-        &mut self,
-        id: &uuid::Uuid,
-        balance: f64,
-        category: String,
-        note: String,
-    ) -> Result<(), EngineError>;
-    fn insert(&mut self, entry: Entry) -> Result<(), EngineError>;
-}
-
-/// An "hard" bounded by `max_balance` Cash Flow.
+/// A "soft" bounded by `max_balance` Cash flow.
 ///
-/// The **sum** of income cannot exceed max_balance. The expenses are ignored from the sum.
+/// The **sum** of income **and** expenses cannot exceed max_balance.
 #[derive(Debug)]
-pub struct HardBounded {
+pub struct Bounded {
     name: String,
     balance: f64,
     max_balance: f64,
-    total_balance: f64,
     entries: Vec<Entry>,
     archived: bool,
 }
 
-impl HardBounded {
+impl Bounded {
     pub fn new(name: String, balance: f64, max_balance: f64) -> Self {
         Self {
             name,
             balance,
             max_balance,
-            total_balance: balance,
             entries: Vec::new(),
             archived: false,
         }
     }
 }
 
-impl CashFlow for HardBounded {
+impl CashFlow for Bounded {
     fn archive(&mut self) {
         self.archived = true
     }
@@ -79,12 +44,9 @@ impl CashFlow for HardBounded {
     }
 
     fn insert(&mut self, entry: Entry) -> Result<(), EngineError> {
-        if entry.amount > 0f64 && self.total_balance + entry.amount > self.max_balance {
+        if entry.amount > 0f64 && self.balance + entry.amount > self.max_balance {
             Err(EngineError::MaxBalanceReached(self.name.clone()))
         } else {
-            if entry.amount > 0f64 {
-                self.total_balance += entry.amount;
-            }
             self.balance += entry.amount;
             self.entries.push(entry);
             Ok(())
@@ -101,16 +63,13 @@ impl CashFlow for HardBounded {
         match self.entries.iter().position(|entry| entry.id == *id) {
             Some(index) => {
                 let entry = &mut self.entries[index];
-                // Check if the entry or the update is an income and if the updates does not
-                // exceed `max_balance`
-                if entry.amount > 0f64 || amount > 0f64 {
-                    if self.total_balance - entry.amount + amount > self.max_balance {
-                        return Err(EngineError::MaxBalanceReached(self.name.clone()));
-                    }
-                    self.total_balance = self.total_balance - entry.amount + amount;
+                let new_balance = self.balance - entry.amount + amount;
+
+                if new_balance > self.max_balance {
+                    return Err(EngineError::MaxBalanceReached(self.name.clone()));
                 }
 
-                self.balance = self.balance - entry.amount + amount;
+                self.balance = new_balance;
                 entry.amount = amount;
                 entry.category = category;
                 entry.note = note;
@@ -126,13 +85,13 @@ impl CashFlow for HardBounded {
 mod tests {
     use super::*;
 
-    fn hard_bounded_flow() -> HardBounded {
-        HardBounded::new(String::from("Cash"), 0f64, 10f64)
+    fn bounded() -> Bounded {
+        Bounded::new(String::from("Cash"), 0f64, 10f64)
     }
 
     #[test]
-    fn add_entry_hard_bounded() {
-        let mut flow = hard_bounded_flow();
+    fn add_entry() {
+        let mut flow = bounded();
         flow.add_entry(1.23, "Income".to_string(), "Weekly".to_string())
             .unwrap();
         let entry = &flow.entries[0];
@@ -145,23 +104,15 @@ mod tests {
 
     #[test]
     #[should_panic(expected = "MaxBalanceReached(\"Cash\")")]
-    fn fail_add_entry_hard_bounded() {
-        let mut flow = hard_bounded_flow();
+    fn fail_add_entry() {
+        let mut flow = bounded();
         flow.add_entry(20.44, "Income".to_string(), "Weekly".to_string())
             .unwrap()
     }
 
     #[test]
-    fn check_hard_bounded_archived() {
-        let mut flow = hard_bounded_flow();
-        assert_eq!(flow.archived, false);
-        flow.archive();
-        assert_eq!(flow.archived, true);
-    }
-
-    #[test]
-    fn delete_entry_hard_bounded() {
-        let mut flow = hard_bounded_flow();
+    fn delete_entry() {
+        let mut flow = bounded();
         flow.add_entry(1.23, "Income".to_string(), "Weekly".to_string())
             .unwrap();
         let entry_id = flow.entries[0].id;
@@ -172,8 +123,8 @@ mod tests {
     }
 
     #[test]
-    fn update_entry_hard_bounded() {
-        let mut flow = hard_bounded_flow();
+    fn update_entry() {
+        let mut flow = bounded();
         flow.add_entry(1.23, "Income".to_string(), "Weekly".to_string())
             .unwrap();
         let entry_id = flow.entries[0].id;
@@ -195,26 +146,9 @@ mod tests {
 
     #[test]
     #[should_panic(expected = "MaxBalanceReached(\"Cash\")")]
-    fn fail_update_entry_hard_bounded() {
-        let mut flow = hard_bounded_flow();
+    fn fail_update_entry() {
+        let mut flow = bounded();
         flow.add_entry(1.23, "Income".to_string(), "Weekly".to_string())
-            .unwrap();
-        let entry_id = flow.entries[0].id;
-
-        flow.update_entry(
-            &entry_id,
-            20f64,
-            String::from("Income"),
-            String::from("Monthly"),
-        )
-        .unwrap();
-    }
-
-    #[test]
-    #[should_panic(expected = "MaxBalanceReached(\"Cash\")")]
-    fn fail_update_income_expense_switch() {
-        let mut flow = hard_bounded_flow();
-        flow.add_entry(-1.23, "Income".to_string(), "Weekly".to_string())
             .unwrap();
         let entry_id = flow.entries[0].id;
 
