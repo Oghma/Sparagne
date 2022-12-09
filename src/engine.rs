3db4989fed93295b0514f5bfec78b29b84ab9a12
Matteo Lisotto <matteo.lisotto@gmail.com>
2022-12-10 00:14:50 +0100

    [engine] Refactor `Engine` type

--- src/engine.rs
+++ src/engine.rs
@@ -1,6 +1,6 @@
 //! This module is the core of the application. The `Engine` struct handles cash
 //! flows and wallets.
-use self::cash_flows::CashFlow;
+use self::{cash_flows::CashFlow, errors::EngineError};
 use std::collections::hash_map::Iter;
 use std::collections::HashMap;
 
@@ -22,13 +22,16 @@ impl Engine {
 
     pub fn add_flow_entry(
         &mut self,
-        cash_flow: String,
+        flow_name: &String,
         amount: f64,
         category: String,
         note: String,
-        let flow = self.chash_flows.get_mut(&cash_flow).unwrap();
     ) -> Result<uuid::Uuid, errors::EngineError> {
-        flow.add_entry(amount, category, note)
+        match self.chash_flows.get_mut(flow_name) {
+            Some(flow) => flow.add_entry(amount, category, note),
+            None => Err(EngineError::KeyNotFound(flow_name.clone())),
+        }
+    }
     }
 
     pub fn new_flow(
@@ -37,16 +40,21 @@ impl Engine {
         balance: f64,
         max_balance: Option<f64>,
         hard_bounded: Option<bool>,
-    ) {
-        let flow: Box<dyn CashFlow> = match max_balance {
-            Some(mxb) => match hard_bounded {
-                Some(true) => Box::new(cash_flows::HardBounded::new(name.clone(), balance, mxb)),
-                _ => Box::new(cash_flows::Bounded::new(name.clone(), balance, mxb)),
-            },
-            _ => Box::new(cash_flows::UnBounded::new(name.clone(), balance)),
-        };
+    ) -> Result<(), errors::EngineError> {
+        if self.chash_flows.contains_key(&name) {
+            return Err(errors::EngineError::ExistingKey(name));
+        }
 
+        let flow: Box<dyn CashFlow> = match (max_balance, hard_bounded) {
+            (Some(mxb), Some(true)) => {
+                Box::new(cash_flows::HardBounded::new(name.clone(), balance, mxb))
+            }
+            (Some(mxb), _) => Box::new(cash_flows::Bounded::new(name.clone(), balance, mxb)),
+            (None, _) => Box::new(cash_flows::UnBounded::new(name.clone(), balance)),
+        };
         self.chash_flows.insert(name, flow);
+
+        Ok(())
     }
 
     pub fn flow_iter(&self) -> Iter<String, Box<dyn CashFlow>> {
