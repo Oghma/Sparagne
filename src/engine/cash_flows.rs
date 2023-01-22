bcbbe86612d5f05dc3138d1dd0969db2ac9849de
Matteo Lisotto <matteo.lisotto@gmail.com>
2023-01-22 22:30:49 +0100

    [cashFlows] Remove `CashFlows` trait and its implementation

--- src/engine/cash_flows.rs
+++ src/engine/cash_flows.rs
@@ -1,18 +1,7 @@
-//! A collection of types that implement `CashFlow` trait.
+//! The module contains the representation of a cash flow.
 //!
-//! The available cash flows are:
-//! - [`UnBounded`]
-//! - [`Bounded`]
-//! - [`HardBounded`]
 use super::entry::Entry;
 use super::errors::EngineError;
-pub use bounded::Bounded;
-pub use hard_bounded::HardBounded;
-pub use unbounded::UnBounded;
-
-mod bounded;
-mod hard_bounded;
-mod unbounded;
 
 /// A cash flow.
 ///
@@ -155,33 +144,4 @@ impl CashFlow {
             None => Err(EngineError::KeyNotFound(id.to_string())),
         }
     }
-}
-
-/// `CashFlow` trait. Base requirements for a `CashFlow`.
-pub trait CashFlow {
-    fn add_entry(
-        &mut self,
-        balance: f64,
-        category: String,
-        note: String,
-    ) -> Result<uuid::Uuid, EngineError> {
-        let entry = Entry::new(balance, category, note);
-        self.insert(entry)
-    }
-
-    fn archive(&mut self);
-
-    fn archived(&self) -> bool;
-
-    fn delete_entry(&mut self, id: &uuid::Uuid) -> Result<(), EngineError>;
-
-    fn update_entry(
-        &mut self,
-        id: &uuid::Uuid,
-        balance: f64,
-        category: String,
-        note: String,
-    ) -> Result<(), EngineError>;
-
-    fn insert(&mut self, entry: Entry) -> Result<uuid::Uuid, EngineError>;
 }
