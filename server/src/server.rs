2463b459916b5f4f3e13f5d89aefd0b79d4237f2
Matteo Lisotto <matteo.lisotto@gmail.com>
2023-08-29 21:26:09 +0200

    [server] Add `TypedHeader` for telegram header

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,5 +1,6 @@
 use axum::{
     extract::State,
+    headers::Header,
     routing::{get, post},
     Router,
 };
@@ -11,7 +12,41 @@ use crate::cash_flow;
 use crate::entry;
 use engine::Engine;
 
+static TELEGRAM_HEADER: axum::http::HeaderName =
+    axum::http::HeaderName::from_static("telegram-user-id");
+
 pub type SharedState = State<Arc<RwLock<Engine>>>;
+
+/// `TypedHeader` for custom telegram header
+#[derive(Debug)]
+struct TelegramHeader(u64);
+
+impl Header for TelegramHeader {
+    fn name() -> &'static axum::http::HeaderName {
+        &TELEGRAM_HEADER
+    }
+
+    fn decode<'i, I>(values: &mut I) -> Result<Self, axum::headers::Error>
+    where
+        Self: Sized,
+        I: Iterator<Item = &'i axum::http::HeaderValue>,
+    {
+        let value = values.next().ok_or_else(axum::headers::Error::invalid)?;
+        let Ok(value) = value.to_str() else {
+            return Err(axum::headers::Error::invalid());
+        };
+        let Ok(value) = value.parse() else {
+            return Err(axum::headers::Error::invalid());
+        };
+
+        Ok(TelegramHeader(value))
+    }
+
+    fn encode<E: Extend<axum::http::HeaderValue>>(&self, values: &mut E) {
+        let value = axum::http::HeaderValue::from_str(&self.0.to_string()).unwrap();
+        values.extend(std::iter::once(value));
+    }
+}
 
 pub async fn run(engine: Engine) {
     let state = Arc::new(RwLock::new(engine));
