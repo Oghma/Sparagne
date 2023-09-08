fd46917cc81c532afe4c7b6723247591ae260db0
Matteo Lisotto <matteo.lisotto@gmail.com>
2023-09-08 22:12:44 +0200

    [app] Rewrite `main`

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,58 @@
 use engine;
 use server;
+use settings::Database;
 use telegram_bot;
 
 mod settings;
 
 #[tokio::main]
 async fn main() {
+    let settings = settings::Settings::new().unwrap();
+    let mut tasks = tokio::task::JoinSet::new();
+
     tracing_subscriber::fmt()
-        .with_env_filter("hodlTracker=debug,telegram_bot=debug")
+        .with_env_filter(format!(
+            "hodlTracker={level},telegram_bot={level},server={level}",
+            level = settings.app.level
+        ))
         .init();
 
-    let settings = settings::Settings::new().unwrap();
+    if let Some(server) = settings.server {
+        tasks.spawn(async move {
+            tracing::info!("Found server settings...");
+            let db = parse_database(&server.database).await;
+
+            let engine = engine::Engine::builder()
+                .database(db.clone())
+                .initialize()
+                .build()
+                .await;
+            server::run(engine, db).await;
+        });
+    }
 
     if let Some(telegram) = settings.telegram {
-        tracing::info!("Found telegram settings...");
-        telegram_bot::Bot::builder()
-            .token(&telegram.token)
-            .server(&telegram.server)
-            .build()
-            .run()
-            .await;
+        tasks.spawn(async move {
+            tracing::info!("Found telegram settings...");
+            telegram_bot::Bot::builder()
+                .token(&telegram.token)
+                .server(&telegram.server)
+                .build()
+                .run()
+                .await;
+        });
     }
+
+    while let Some(_) = tasks.join_next().await {}
+}
+
+async fn parse_database(config: &settings::Database) -> sea_orm::DatabaseConnection {
+    let url = match config {
+        Database::Memory => String::from("sqlite::memory"),
+        Database::Sqlite(path) => format!("sqlite:{}", path),
+    };
+
+    sea_orm::Database::connect(url)
+        .await
+        .expect("Failed to connect to the database")
 }
