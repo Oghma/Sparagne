325378619debfcaa571766832f62659a2695d049
Matteo Lisotto <matteo.lisotto@gmail.com>
2023-11-01 17:33:31 +0100

    [server] Add `ServerError`

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,6 +1,30 @@
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use engine::EngineError;
+
+use serde::Serialize;
 pub use server::run;
 
 mod cash_flow;
 mod entry;
 mod server;
 mod user;
+
+pub struct ServerError(EngineError);
+
+//TODO: Find a better solution
+#[derive(Serialize)]
+struct Error {
+    error: String,
+}
+
+impl IntoResponse for ServerError {
+    fn into_response(self) -> axum::response::Response {
+        (
+            StatusCode::BAD_REQUEST,
+            Json(Error {
+                error: format!("{}", self.0),
+            }),
+        )
+            .into_response()
+    }
+}
