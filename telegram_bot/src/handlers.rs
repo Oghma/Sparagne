1cf4371d3e6be0172ba948cbe5aa2bc2d2a7e0e1
Matteo Lisotto <matteo.lisotto@gmail.com>
2024-01-11 16:46:35 +0100

    fix(telegram): fix `unpair` command

--- telegram_bot/src/handlers.rs
+++ telegram_bot/src/handlers.rs
@@ -86,21 +86,20 @@ pub async fn handle_pair_user(
     msg: Message,
     cmd: HandleUserAccount,
 ) -> ResponseResult<()> {
+    let telegram_id = msg.from().map(|user| user.id.to_string()).unwrap();
+
     match cmd {
         HandleUserAccount::Pair { code } => {
             let response = cfg
                 .client
                 .post(cfg.server.to_string() + "/user/pair")
-                .json(&server::types::user::PairUser {
-                    code,
-                    telegram_id: msg.from().map(|user| user.id.to_string()).unwrap(),
-                })
+                .json(&server::types::user::PairUser { code, telegram_id })
                 .send()
                 .await
                 .unwrap();
 
             let user_response = match response.status() {
-                StatusCode::ACCEPTED => "Account paired",
+                StatusCode::CREATED => "Account paired",
                 _ => {
                     tracing::debug!("{:?}", response);
                     tracing::debug!("body: {}", response.text().await.unwrap());
@@ -112,8 +111,24 @@ pub async fn handle_pair_user(
             bot.send_message(msg.chat.id, user_response).await?;
         }
         HandleUserAccount::UnPair => {
-            cfg.client.delete(cfg.server + "/pairUser").send().await?;
-            bot.send_message(msg.chat.id, "Account unpaired").await?;
+            let response = cfg
+                .client
+                .delete(cfg.server + "/user/pair")
+                .header("telegram-user-id", telegram_id)
+                .send()
+                .await
+                .unwrap();
+
+            let user_response = match response.status() {
+                StatusCode::ACCEPTED => "Account unpaired",
+                _ => {
+                    tracing::debug!("{:?}", response);
+                    tracing::debug!("body: {}", response.text().await.unwrap());
+
+                    "Connection problems with the server. Retry later!"
+                }
+            };
+            bot.send_message(msg.chat.id, user_response).await?;
         }
     }
 
