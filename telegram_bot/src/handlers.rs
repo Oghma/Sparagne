b77d942ab7d4644f4923faaf45a960a55d631918
Matteo Lisotto <matteo.lisotto@gmail.com>
2024-02-20 22:18:16 +0100

    feat(telegram): add `GlobalState` and `GlobalDialogue`

--- telegram_bot/src/handlers.rs
+++ telegram_bot/src/handlers.rs
@@ -1,4 +1,15 @@
 //! Handlers for managing commands
 
+use teloxide::{dispatching::dialogue::InMemStorage, prelude::Dialogue};
+
 pub mod entry;
 pub mod user;
+
+#[derive(Debug, Default, Clone)]
+pub enum GlobalState {
+    #[default]
+    Idle,
+    InDelete(Vec<(String, String)>),
+}
+
+type GlobalDialogue = Dialogue<GlobalState, InMemStorage<GlobalState>>;
